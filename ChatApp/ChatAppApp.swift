db54b41b314b8f6bbb0229e566c5d8933821cedd
joaobug <joaoenfrance@gmail.com>
2024-02-08 17:35:09 -0300

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Joao Leal on 08/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
